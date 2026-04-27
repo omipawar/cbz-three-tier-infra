b54091d2f5fac1df9213eb461f21a35acd1d567f
Ubuntu <ubuntu@ip-172-31-33-183.ap-south-1.compute.internal>
2026-04-27 16:54:01 +0000

    updated infra files

--- main.tf
+++ main.tf
@@ -1,20 +1,20 @@
 provider "aws" {
-    region = "us-east-2"
+  region = "ap-south-1"
 }
 
 module "rds" {
-    source = "./modules/rds"
+  source = "./modules/rds"
 }
 
 module "eks" {
-    source = "./modules/eks"
-    project = "cbz"
-    desired_nodes = 2
-    max_nodes  = 2
-    min_nodes  = 2
-    node_instance_type = "t3.medium"
+  source             = "./modules/eks"
+  project            = "cbz"
+  desired_nodes      = 2
+  max_nodes          = 2
+  min_nodes          = 2
+  node_instance_type = "t3.small"
 }
 
 module "s3" {
-    source = "./modules/s3"
+  source = "./modules/s3"
 }
